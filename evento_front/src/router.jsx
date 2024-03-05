2bfcf2e67f24147ef3fb49a0c7f0b27c47352ca3
MedHachami <moha.hach.9@gmail.com>
2024-03-05 12:50:41 +0100

    app router

---
+++ evento_front/src/router.jsx
@@ -0,0 +1,49 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import GuestLayout from "./Layouts/GuestLayout";
+import DefaultLayout from "./Layouts/DefaultLayout";
+import Login from "./UI/Login";
+import Register from "./UI/Register";
+import Home from "./Pages/Home";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element:<DefaultLayout />,
+        children:[
+            {
+                path:'/Home',
+                element : <Home />,
+            },
+            // {
+            //     path:'/',
+            //     element : <Dashboard />,
+            // },
+            // {
+            //     path:'/surveys',
+            //     element : <Survey />,
+            // },
+            // {
+            //     path:'/surveys/create',
+            //     element : <SurveyView />,
+            // },
+        ]
+    },
+    {
+        path:'/',
+        element:<GuestLayout />,
+        children:[
+            {
+                path:'login',
+                element : <Login />,
+            },
+            {
+                path:'signup',
+                element : <Register />,
+            }
+        ],
+    }
+])
+
+export default router;
