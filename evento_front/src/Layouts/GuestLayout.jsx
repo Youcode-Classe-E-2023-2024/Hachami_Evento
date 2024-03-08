aeba3062645e030c5eca43bf27f0afd2ba5f8190
MedHachami <moha.hach.9@gmail.com>
2024-03-08 03:15:47 +0100

    Update GuestLayout.jsx

--- evento_front/src/Layouts/GuestLayout.jsx
+++ evento_front/src/Layouts/GuestLayout.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from "../contexts/ContextProvider";
 
 
 const GuestLayout = () => {
+    const { userToken } = useStateContext();
+
+    if (userToken) {
+        return <Navigate to="/" />
+    }
     return (
         <div >
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <h1 className="text-center text-[#fc444a] font-light sm:text-[23px] text-[19px] xxl:text-[40px] flex-1">
                         <span className="text-white">Evento</span> Ticket
-                    </h1>       
+                    </h1>
                 </div>
 
                 <Outlet />
