ce7f53ca39e6f395228c4a65383121fe6d68304c
MedHachami <moha.hach.9@gmail.com>
2024-03-08 03:15:41 +0100

    Update DefaultLayout.jsx

--- evento_front/src/Layouts/DefaultLayout.jsx
+++ evento_front/src/Layouts/DefaultLayout.jsx
@@ -1,20 +1,43 @@
-import { React } from 'react'
+import { React, useEffect } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 
 import NavBar from '../UI/NavBar'
 import HeroImg from '../assets/hero.png'
+import { useStateContext } from '../contexts/ContextProvider'
+import axiosClient from "../axios";
 
 
 
 
 
 export default function DefaultLayout() {
+    const { events, setEvents , query ,category } = useStateContext();
+    
+    
+    
+  
+
+    useEffect(() => {
+        axiosClient
+            .get("/events")
+            .then(({ data }) => {
+                console.log(query,category);
+                setEvents(data);
+
+
+            })
+            .catch((error) => {
+
+                console.error(error);
+            });
+    }, [query,category])
+
 
     return (
         <div className='flex flex-col ' style={{ 'height': '100vh' }}>
             <NavBar />
             <Outlet />
-            
+
 
         </div>
     )
