a601868b035b29a4ec9aad96fe4c8993a4ccb57b
MedHachami <moha.hach.9@gmail.com>
2024-03-08 03:16:46 +0100

    Update index.js

--- evento_front/src/index.js
+++ evento_front/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { RouterProvider } from 'react-router-dom'
 import router from './router';
+import { ContextProvider } from './contexts/ContextProvider.jsx'
+
 
 ReactDOM.render(
     <React.StrictMode>
+        <ContextProvider>
         <RouterProvider router={router} />
+        </ContextProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
